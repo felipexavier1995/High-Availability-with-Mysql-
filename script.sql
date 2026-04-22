28ebee0d2e994ef8c919da4f45dceec496309552
Felipe Bento Xavier <52262336+felipexavier1995@users.noreply.github.com>
2026-04-21 21:44:58 -0300

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,23 @@
+--- Commands sql and shell
+/*
+Here have alls commands that used on Project.
+*/
+
+--- 1 - First alteration is update SO with command:
+	sudo apt update
+--- 2 -  After update, we need install Mysql with command:
+sudo apt install mysql-server -y
+---- 3 - Check all service of mysql with command:
+sudo systemctl status mysql 
+
+
+--- Now we can edit in path file
+
+vi /etc/mysql/mysql.conf.d/mysqld.cnf
+
+-- Command in SQL, We can created a user and password with your grants
+-- PS switch the name 'senha' for other password
+CREATE USER 'repl'@'%' IDENTIFIED BY 'senha';
+GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
+FLUSH PRIVILEGES;
+
